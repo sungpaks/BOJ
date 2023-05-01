ffe897278590b08e69b27d3ec0637ebe9607ac92
sungpaks <122661764+sungpaks@users.noreply.github.com>
2023-05-01 20:29:03 +0900

    [Silver III] Title: N과 M (1), Time: 24 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int>v;
+
+void MnNs(int n, int m, int current);
+int main(){
+	
+	ios_base :: sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	
+	int n, m;
+	cin >> n >> m;
+	for(int i=0;i<m;i++){
+		v.push_back(0);
+	}
+	MnNs(n,m,0);
+}
+
+void MnNs(int n, int m, int current)
+{
+	if (current == m)
+	{
+		for(int i=0;i<m;i++) cout << v[i] << " ";
+		cout << "\n";
+		return;
+	}
+	for(int i=1;i<=n;i++){
+		v[current] = i;
+		int flag = 1;
+		for(int j=0;j<current;j++){
+			if (v[j] == v[current]) flag = 0;
+		}
+		if (flag == 1) MnNs(n, m, current+1);
+	}
+}
