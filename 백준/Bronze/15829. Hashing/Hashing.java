7aea3161065874075904ca37c854f08c88062f6a
sungpaks <122661764+sungpaks@users.noreply.github.com>
2023-07-31 18:53:05 +0900

    [Bronze II] Title: Hashing, Time: 124 ms, Memory: 14196 KB -BaekjoonHub

---
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -0,0 +1,19 @@
+
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		String str = br.readLine();
+		long result = 0;
+		long pow = 1;
+		for(int i=0;i<n;i++) {
+			result += ((str.charAt(i)-'a'+1) * pow);
+			pow *= 31 % 1234567891;
+		}
+		System.out.println(result%1234567891);
+	}
+}
